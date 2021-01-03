2c75ab0eec0fa8ff415149c3c40d0e62fc0b0ec4
uhyo <uhyo@uhy.ooo>
2021-01-03 17:07:35 +0900

    install locally

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": 12,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "@typescript-eslint",
+    "local-rules"
+  ],
+  "rules": {
+    "no-undef": ["off"],
+    "local-rules/scope": ["error"]
+  }
+};
